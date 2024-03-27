b68118c091fd63371f0028c4f73c32b27dbc2b61
Sambat Das <revosambat123@gmail.com>
2024-03-27 19:22:48 +0530

    Web title component for reusable title in GeneralLayout

---
+++ src/components/WebTitle.tsx
@@ -0,0 +1,13 @@
+import { Box, Typography } from "@mui/material"
+
+const WebTitle = () => {
+	return (
+		<Box>
+			<Typography sx={{ padding: "30px", width: "100%" }} variant="h1">
+				<i>CV-Generator</i>
+			</Typography>
+		</Box>
+	)
+}
+
+export default WebTitle
