d62bf43a14f88ee6158a31d45c77f6147b09f207
Sambat Das <revosambat123@gmail.com>
2024-04-08 16:17:35 +0530

    unecessary code removed

--- src/containers/SharedLayout/index.tsx
+++ src/containers/SharedLayout/index.tsx
@@ -18,36 +18,7 @@ const index = () => {
 		>
 			{router.pathname == "/" ? (
 				<>
-					<Typography
-						padding="20px"
-						textAlign="justify"
-						variant="h2"
-						marginTop="auto"
-						letterSpacing={"3px"}
-						lineHeight={"30px"}
-					>
-						Welcome to our cutting-edge CV Generator, where crafting the perfect
-						resume is a breeze. Whether you're a seasoned professional or just
-						starting your career journey, our platform is designed to empower
-						you with the tools needed to showcase your skills, experiences, and
-						achievements effectively.
-					</Typography>
-					<br />
 					<CustomButton text={"Create CV"} handleClick={handleClick} />
-					<br />
-					<Typography
-						padding={"20px"}
-						textAlign="justify"
-						variant="h2"
-						letterSpacing={"3px"}
-						lineHeight={"30px"}
-					>
-						Gone are the days of struggling with formatting and design. Our
-						intuitive interface guides you through the process, allowing you to
-						create a polished and professional CV in minutes. With a wide range
-						of templates to choose from, you can tailor your resume to suit your
-						industry and personal style.
-					</Typography>
 				</>
 			) : router.pathname === "/cv-form" ? (
 				<CVHeaderList />
