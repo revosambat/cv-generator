e9d7e35c2cbcdeb97cf3a4a2fe93e0fbd9ccc6b2
Sambat Das <revosambat123@gmail.com>
2024-04-08 16:14:27 +0530

    Type define refactoring changes

--- src/containers/CvForm/index.tsx
+++ src/containers/CvForm/index.tsx
@@ -1,11 +1,13 @@
 import { useContext } from "react"
 import { FormDataContext } from "../../context/context"
-import GeneralInformation from "./components/GeneralInformation"
-import AcademicQualification from "./components/AcademicQualification"
-import WorkExperience from "./components/WorkExperience"
-import SkillHighlights from "./components/SkillHighlights"
-import Projects from "./components/Projects"
-import Languages from "./components/Languages"
+import GeneralInformation from "./tabPanels/GeneralInformation"
+import AcademicQualification from "./tabPanels/AcademicQualification"
+import WorkExperience from "./tabPanels/WorkExperience"
+import SkillHighlights from "./tabPanels/SkillHighlights"
+import Projects from "./tabPanels/Projects"
+import Languages from "./tabPanels/Languages"
+import { CVHeaderData } from "../../contants"
+import { Box } from "@mui/material"
 import { cvDataTypes } from "../../context/store"
 
 const CvForm = () => {
@@ -24,47 +26,98 @@ const CvForm = () => {
 	const handleSubmit = (): void => {}
 	const updateForm = (
 		updatedData:
-			| { generalInformation: cvDataTypes["generalInformation"] }
-			| { academicQualification: cvDataTypes["academicQualification"] }
-			| { workExperience: cvDataTypes["workExperience"] }
-			| { skillHighlights: cvDataTypes["skillHighlights"] }
-			| { projects: cvDataTypes["projects"] }
-			| { languages: cvDataTypes["languages"] }
+			| { activeFormSlug: cvDataTypes["activeFormSlug"] }
+			| {
+					generalInformation: cvDataTypes["generalInformation"]
+			  }
+			| {
+					academicQualification: cvDataTypes["academicQualification"]
+			  }
+			| {
+					workExperience: cvDataTypes["workExperience"]
+			  }
+			| {
+					projects: cvDataTypes["projects"]
+			  }
+			| {
+					languages: cvDataTypes["languages"]
+			  }
+			| {
+					skillHighlights: cvDataTypes["skillHighlights"]
+			  }
 	) => {
 		dispatch({
 			type: "onUpdate",
-			payload: { ...initData, ...updatedData },
+			payload: { ...updatedData },
+		})
+	}
+	const tabChange = (changeFlag: number) => {
+		const totalIdx = CVHeaderData.length - 1
+		// const {activeFormSlug} = initData
+		let activeSlugIdx = CVHeaderData.findIndex(
+			({ slug }) => slug == initData.activeFormSlug
+		)
+		activeSlugIdx = activeSlugIdx + changeFlag
+		if (activeSlugIdx < 0) {
+			activeSlugIdx = 0
+		}
+		if (activeSlugIdx > totalIdx) {
+			return
+		}
+		dispatch({
+			type: "onSelect",
+			payload: {
+				activeFormSlug: CVHeaderData[activeSlugIdx].slug,
+			},
 		})
 	}
 	return (
-		<>
+		<Box padding={5} height={"100%"} width={"100%"}>
 			<form onSubmit={handleSubmit}>
 				{activeFormSlug === "GenInfo" && (
 					<GeneralInformation
 						formData={generalInformation}
 						updateForm={updateForm}
+						tabChange={tabChange}
 					/>
 				)}
 				{activeFormSlug === "AcdQua" && (
 					<AcademicQualification
 						formData={academicQualification}
 						updateForm={updateForm}
+						tabChange={tabChange}
 					/>
 				)}
 				{activeFormSlug === "WorkExp" && (
-					<WorkExperience formData={workExperience} updateForm={updateForm} />
+					<WorkExperience
+						formData={workExperience}
+						updateForm={updateForm}
+						tabChange={tabChange}
+					/>
 				)}
 				{activeFormSlug === "SkillHigh" && (
-					<SkillHighlights formData={skillHighlights} updateForm={updateForm} />
+					<SkillHighlights
+						formData={skillHighlights}
+						updateForm={updateForm}
+						tabChange={tabChange}
+					/>
 				)}
 				{activeFormSlug === "Proj" && (
-					<Projects formData={projects} updateForm={updateForm} />
+					<Projects
+						formData={projects}
+						updateForm={updateForm}
+						tabChange={tabChange}
+					/>
 				)}
 				{activeFormSlug === "Lang" && (
-					<Languages formData={languages} updateForm={updateForm} />
+					<Languages
+						formData={languages}
+						updateForm={updateForm}
+						tabChange={tabChange}
+					/>
 				)}
 			</form>
-		</>
+		</Box>
 	)
 }
 
