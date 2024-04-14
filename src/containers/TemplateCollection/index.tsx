ab9433caf5b1d86686cbbf8dacb2df2a510a1a2b
Sambat Das <revosambat123@gmail.com>
2024-04-14 16:42:29 +0530

    Template Collection boiler plate added to display the templates of CV

--- src/containers/TemplateCollection/index.tsx
+++ src/containers/TemplateCollection/index.tsx
@@ -1,7 +1,44 @@
-import React from "react"
+import React, { useContext, useState } from "react"
+import { FormDataContext } from "../../context/context"
+import CvViewer from "./components/CvViewer"
+import { Box, Grid, Paper, styled } from "@mui/material"
+
+const Item = styled(Paper)`
+	width: 200px;
+	height: 250px;
+`
 
 const TemplateCollection = () => {
-	return <div>TemplateCollection</div>
+	const formContext = useContext(FormDataContext)
+	const [open, setOpen] = useState(true)
+
+	const handleModalClose = () => {
+		setOpen(false)
+	}
+	console.log(formContext)
+	return (
+		<Box bgcolor={"#CFDAEE"} width={"100vw"} height={"100vh"}>
+			<Box
+				width={800}
+				height={600}
+				overflow={"scroll"}
+				position={"absolute"}
+				top={"15%"}
+				left={"25%"}
+			>
+				<Grid container gap={8}>
+					{Array(30)
+						.fill("templates")
+						.map((val) => (
+							<Grid item>
+								<Item>{val}</Item>
+							</Grid>
+						))}
+				</Grid>
+			</Box>
+			<CvViewer open={open} handleClose={handleModalClose} />
+		</Box>
+	)
 }
 
 export default TemplateCollection
