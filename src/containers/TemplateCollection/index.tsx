084a2fe54c2d7acd2c19c469a359db8d614e79c4
Sambat Das <revosambat123@gmail.com>
2024-04-15 17:58:58 +0530

    template collection page implementation for choosing the templates

--- src/containers/TemplateCollection/index.tsx
+++ src/containers/TemplateCollection/index.tsx
@@ -1,44 +1,103 @@
-import React, { useContext, useState } from "react"
+import { useContext, useRef, useState } from "react"
 import { FormDataContext } from "../../context/context"
 import CvViewer from "./components/CvViewer"
-import { Box, Grid, Paper, styled } from "@mui/material"
+import { Box, Paper, styled, IconButton, Typography } from "@mui/material"
+import ArrowCircleLeftIcon from "@mui/icons-material/ArrowCircleLeft"
+import ArrowCircleRightIcon from "@mui/icons-material/ArrowCircleRight"
 
 const Item = styled(Paper)`
-	width: 200px;
+	min-width: 200px;
 	height: 250px;
-	@media only screen and (max-width: 670px) {
-		width: 300px;
-		height: 380px;
+	margin: auto;
+	&:hover {
+		transition: all 300ms ease-in;
+		transform: scale(1.2);
+		opacity: 1;
+		box-shadow: 2px 4px 5px 0px #5b7492;
 	}
 `
 
 const TemplateCollection = () => {
 	const formContext = useContext(FormDataContext)
 	const [open, setOpen] = useState(true)
+	const scrollRef = useRef<HTMLElement>(null)
 
 	const handleModalClose = () => {
 		setOpen(false)
 	}
+
+	const handleScroll = (offset: number) => {
+		if (scrollRef.current) {
+			scrollRef.current.scrollLeft += offset
+		}
+	}
 	console.log(formContext)
 	return (
-		<Box bgcolor={"#CFDAEE"} width={"100vw"} height={"100vh"}>
+		<Box
+			width={"100vw"}
+			height={"100vh"}
+			display={"flex"}
+			flexDirection={"column"}
+			p={10}
+		>
+			<Typography variant="h1" textAlign={"left"} p={5}>
+				Choose your template!
+			</Typography>
 			<Box
-				width={"80%"}
-				height={600}
-				overflow={"scroll"}
-				position={"absolute"}
-				top={"15%"}
-				left={"16%"}
+				width={"100%"}
+				display={"flex"}
+				bgcolor={"#CFDAEE"}
+				borderRadius={10}
 			>
-				<Grid container gap={8}>
-					{Array(30)
+				<Box
+					display={"flex"}
+					flex={0.5}
+					justifyContent={"center"}
+					alignItems={"center"}
+				>
+					<IconButton onClick={() => handleScroll(-500)}>
+						<ArrowCircleLeftIcon fontSize="large" />
+					</IconButton>
+				</Box>
+				<Box
+					display={"flex"}
+					flexWrap={"nowrap"}
+					overflow={"scroll"}
+					ref={scrollRef}
+					sx={{ scrollBehavior: "smooth" }}
+					justifyContent={"space-between"}
+					gap={8}
+					flex={2.5}
+					p={10}
+				>
+					{Array(10)
 						.fill("templates")
 						.map((val) => (
-							<Grid item>
-								<Item>{val}</Item>
-							</Grid>
+							<Item>{val}</Item>
 						))}
-				</Grid>
+				</Box>
+				<Box
+					display={"flex"}
+					flex={0.5}
+					alignItems={"center"}
+					justifyContent={"center"}
+				>
+					<IconButton onClick={() => handleScroll(+500)}>
+						<ArrowCircleRightIcon fontSize="large" />
+					</IconButton>
+				</Box>
+			</Box>
+			<Box p={5}>
+				<Typography variant="body2">
+					<strong>Introducing our CV Generator:</strong> The hassle-free
+					solution for crafting professional resumes. With our user-friendly
+					interface, you can effortlessly create your perfect CV in minutes.
+					Simply input your information, select from a variety of sleek
+					templates tailored to your industry, and customize to your heart's
+					content. Once you're satisfied, choose to print, download, or view
+					your CV online. Say goodbye to the stress of formatting – with our CV
+					Generator, landing your dream job just got a whole lot easier.
+				</Typography>
 			</Box>
 			<CvViewer open={open} handleClose={handleModalClose} />
 		</Box>
